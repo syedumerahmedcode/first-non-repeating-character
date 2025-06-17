4fe9e20924281d9fd65f2913c0704994025f7815
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-06-17 19:41:13 +0200

    Feature: Added comments in the method which highlights the logic used.

--- src/com/umer/service/FirstNonRepeatingCharacterService.java
+++ src/com/umer/service/FirstNonRepeatingCharacterService.java
@@ -3,6 +3,25 @@ package com.umer.service;
 public class FirstNonRepeatingCharacterService {
 
     public Character findFirstNonRepeatingCharacter(String inputToCheck) {
+        // Precheck 1: If inputToCheck=null, return null.
+
+        // Precheck 2: If inputToCheck=""(empty string), return null.-->Reason: minimum
+        // length should be 2. This is the assumption that we make for the logic.
+
+        // Precheck 3: If inputToCheck.legth=1([just one charcter]), return
+        // null.-->Reason: same as pre check 2
+
+        // Step 1: Create an array to record the occurance of each character
+        // NOTE: We assume that the input string contains ASCII characters
+
+        // Step 2: Count the occurrance of each character
+
+        // Step 3: Check for the first occurrance of a character where count =
+        // 1(non-repeating).
+        // Side note: if count >1 then the check is effectively for the first *Repeating
+        // character*.
+
+        // Step 4: Return null if no non-repeating characters are found.
         return null;
     }
 
